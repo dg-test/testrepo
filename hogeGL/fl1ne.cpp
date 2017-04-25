f53c77127122c095aec056dd8dca90f3d9c770c3
Tomoki "FL1NE" SHISHIKURA <tomoki.shishikura@gmail.com>
2017-04-25 16:10:41 +0900

    few callbacks added to fl1ne.cpp and header.

--- hogeGL/fl1ne.cpp
+++ hogeGL/fl1ne.cpp
@@ -1,4 +1,16 @@
 #include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 #include "fl1ne.h"
 
+
+void errorCallback(int error, const char* description){
+    std::cerr << "[ERR] " << description << std::endl;
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods){
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS){
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
--- hogeGL/fl1ne.h
+++ hogeGL/fl1ne.h
@@ -1,6 +1,7 @@
 #ifndef __FL1NE_H_INCLUDED__
 #define __FL1NE_H_INCLUDED__
 
-
+void errorCallback(int, const char*);
+void keyCallback(GLFWwindow*, int, int, int, int);
 
 #endif
