22ac8c1f742f25649dcb5ac0c13b8289a95a2fc3
Tomoki "FL1NE" SHISHIKURA <tomoki.shishikura@gmail.com>
2017-04-25 15:25:37 +0900

    test.cpp modified

--- test.cpp
+++ test.cpp
@@ -2,6 +2,8 @@
 
 int main(int argc, char const* argv[])
 {
-    std::cout << "Hello World!" << std::endl;
+    for(int i = 0; i < 10; ++i){
+        std::cout << "The Hello from Underground. Count : " << i << std::endl;
+    }
     return 0;
 }
